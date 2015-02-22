3845df479da9a16bcf0948a17368e8e910bba56f
James Messinger <james.messinger@gmail.com>
2015-02-22 14:57:27 -0600

    Updated "npm run release" script

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,12 @@ var gulp         = require('gulp'),
     uglify       = require('gulp-uglify'),
     source       = require('vinyl-source-stream'),
     buffer       = require('vinyl-buffer'),
-    browserify   = require('browserify'),
-    prompt       = require('gulp-prompt'),
-    util         = require('util'),
-    ChildProcess = require('child_process');
+    browserify   = require('browserify');
 
 
 // Top-level tasks
 gulp.task('default', ['build']);
 gulp.task('build', ['browserify', 'uglify']);
-gulp.task('bump', ['prompt-and-bump']);
 
 
 /**
@@ -60,40 +56,3 @@ function getBrowserifyStream() {
         .pipe(buffer())
         .pipe(sourcemaps.init({loadMaps: true}));
 }
-
-
-/**
- * Prompts for the type of version bump to do, and then runs "npm version" accordingly.
- */
-gulp.task('prompt-and-bump', function(done) {
-    var pkg = require('./package.json');
-    var version = pkg.version.match(/(\d+)\.(\d+)\.(\d+)/);
-    var major = util.format('%d.%d.%d', parseInt(version[1]) + 1, version[2], version[3]);
-    var minor = util.format('%d.%d.%d', version[1], parseInt(version[2]) + 1, version[3]);
-    var patch = util.format('%d.%d.%d', version[1], version[2], parseInt(version[3]) + 1);
-
-    console.log('\n%s is currently on version %s', pkg.name, pkg.version);
-    gulp.src('package.json')
-        .pipe(prompt.prompt({
-                type: 'list',
-                name: 'bump',
-                message: 'How would you like to bump it?',
-                default: 'patch',
-                choices: [
-                    {value: 'patch', name: 'patch (' + patch + ')'},
-                    {value: 'minor', name: 'minor (' + minor + ')'},
-                    {value: 'major', name: 'major (' + major + ')'}
-                ]
-            },
-            function(answer) {
-                ChildProcess.exec('npm version ' + answer.bump, function(err, stdout, stderr) {
-                    console.log(stdout);
-                    if (err) {
-                        console.error(stderr);
-                        process.exit(1);
-                    }
-                });
-            }
-        ));
-});
-
