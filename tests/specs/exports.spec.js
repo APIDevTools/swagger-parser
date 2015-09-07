ae6f4112ac4ed98e27a1954036c75a3cbc802b24
James Messinger <james.messinger@gmail.com>
2015-09-07 00:52:37 -0500

    Added basic tests to verify that the API is properly exported

---
+++ tests/specs/exports.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+
+describe('Exports', function() {
+  it('should export the SwaggerParser class', function() {
+    expect(SwaggerParser).to.be.a('function');
+  });
+
+  it('should export the YAML object', function() {
+    expect(SwaggerParser.YAML).to.be.an('object');
+    expect(SwaggerParser.YAML.parse).to.be.a('function');
+    expect(SwaggerParser.YAML.stringify).to.be.a('function');
+  });
+
+  it('should export all the static methods of $RefParser', function() {
+    expect(SwaggerParser.parse).to.be.a('function');
+    expect(SwaggerParser.resolve).to.be.a('function');
+    expect(SwaggerParser.bundle).to.be.a('function');
+    expect(SwaggerParser.dereference).to.be.a('function');
+  });
+
+  it('should export the validate method', function() {
+    expect(SwaggerParser.validate).to.be.a('function');
+  });
+});
