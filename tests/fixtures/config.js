e4c49cd8e8df40af4d2daae57123ca0afbb1ea07
James Messinger <james.messinger@gmail.com>
2015-09-28 15:40:15 -0500

    Increasing the test timeout for slow environments (TravisCI and SauceLabs)

--- tests/fixtures/config.js
+++ tests/fixtures/config.js
@@ -15,6 +15,11 @@
     // so we need to increase the timeouts to allow for that
     this.currentTest.timeout(2000);
     this.currentTest.slow(500);
+
+    if (global.__karma__) {
+      // TravisCI and SauceLabs are VERY slow, so we need to increase the test timeout
+      this.currentTest.timeout(10000);
+    }
   });
 
 })();
