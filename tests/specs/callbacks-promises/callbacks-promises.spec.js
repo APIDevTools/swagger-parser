209a70937c2e4128350a94f9a1e8cd433b19f797
James Messinger <james.messinger@gmail.com>
2015-09-28 14:05:25 -0500

    Fixed some tests that were occasionally failing in IE

--- tests/specs/callbacks-promises/callbacks-promises.spec.js
+++ tests/specs/callbacks-promises/callbacks-promises.spec.js
@@ -1,15 +1,15 @@
 'use strict';
 
 describe('Callback & Promise syntax', function() {
-  // NOTE: This delay is so high because TravisCI and SauceLabs,
-  // both of which are VERY SLOW
-  var delay = global.__karma__ ? 2000 : 200;
+  // We need a delay to allow the callback functions to be called asynchronously.
+  var delayMS = 200;
+
+  function delay(fn) {
+    setTimeout(fn, delayMS);
+  }
 
   beforeEach(function() {
-    // These tests all have a delay, to ensure that all callbacks and promises are called.
-    // So we need to increase the test timeouts
-    this.currentTest.timeout(delay * 2);
-    this.currentTest.slow(delay * 2 + 50);
+    this.currentTest.slow(delayMS * 2);
   });
 
   ['parse', 'resolve', 'dereference', 'bundle', 'validate'].forEach(function(method) {
@@ -32,35 +32,34 @@ describe('Callback & Promise syntax', function() {
 
       SwaggerParser[method](path.rel('specs/callbacks-promises/callbacks-promises.yaml'), callbackFn)
         .then(thenFn)
-        .catch(catchFn);
+        .catch(catchFn)
+        .then(delay(function() {
+          try {
+            // Make sure the correct functions were called
+            sinon.assert.calledOnce(callbackFn);
+            sinon.assert.calledOnce(thenFn);
+            sinon.assert.notCalled(catchFn);
 
-      setTimeout(function() {
-        try {
-          // Make sure the correct functions were called
-          sinon.assert.calledOnce(callbackFn);
-          sinon.assert.calledOnce(thenFn);
-          sinon.assert.notCalled(catchFn);
+            // Make sure they were called with the same value
+            var thenFnArg = thenFn.firstCall.args[0];
+            var callbackFnArg = callbackFn.firstCall.args[1];
+            expect(thenFnArg).to.equal(callbackFnArg);
+            sinon.assert.calledWithExactly(callbackFn, null, thenFnArg);
+            sinon.assert.calledWithExactly(thenFn, callbackFnArg);
 
-          // Make sure they were called with the same value
-          var thenFnArg = thenFn.firstCall.args[0];
-          var callbackFnArg = callbackFn.firstCall.args[1];
-          expect(thenFnArg).to.equal(callbackFnArg);
-          sinon.assert.calledWithExactly(callbackFn, null, thenFnArg);
-          sinon.assert.calledWithExactly(thenFn, callbackFnArg);
+            // Make sure the API was parsed correctly
+            if (method !== 'resolve') {
+              var actual = thenFn.firstCall.args[0];
+              var expected = method === 'validate' ? helper.dereferenced.callbacksPromises : helper[method + 'd'].callbacksPromises;
+              expect(actual).to.deep.equal(expected);
+            }
 
-          // Make sure the API was parsed correctly
-          if (method !== 'resolve') {
-            var actual = thenFn.firstCall.args[0];
-            var expected = method === 'validate' ? helper.dereferenced.callbacksPromises : helper[method + 'd'].callbacksPromises;
-            expect(actual).to.deep.equal(expected);
+            done();
           }
-
-          done();
-        }
-        catch (e) {
-          done(e)
-        }
-      }, delay);
+          catch (e) {
+            done(e);
+          }
+        }));
     }
   }
 
@@ -77,29 +76,28 @@ describe('Callback & Promise syntax', function() {
       var parser = new SwaggerParser();
       parser[method](path.rel('specs/callbacks-promises/callbacks-promises.yaml'), callbackFn)
         .then(thenFn)
-        .catch(catchFn);
+        .catch(catchFn)
+        .then(delay(function() {
+          try {
+            // Make sure the correct functions were called
+            sinon.assert.calledOnce(callbackFn);
+            sinon.assert.calledOnce(thenFn);
+            sinon.assert.notCalled(catchFn);
 
-      setTimeout(function() {
-        try {
-          // Make sure the correct functions were called
-          sinon.assert.calledOnce(callbackFn);
-          sinon.assert.calledOnce(thenFn);
-          sinon.assert.notCalled(catchFn);
+            // Make sure they were called with the correct values
+            var result = method === 'resolve' ? parser.$refs : parser.api;
+            var api = method === 'resolve' ? helper.parsed.callbacksPromises : method === 'validate' ? helper.dereferenced.callbacksPromises : helper[method + 'd'].callbacksPromises;
+            expect(parser.api).to.deep.equal(api);
+            expect(parser.$refs.paths()).to.deep.equal([path.abs('specs/callbacks-promises/callbacks-promises.yaml')]);
+            sinon.assert.calledWithExactly(callbackFn, null, result);
+            sinon.assert.calledWithExactly(thenFn, result);
 
-          // Make sure they were called with the correct values
-          var result = method === 'resolve' ? parser.$refs : parser.api;
-          var api = method === 'resolve' ? helper.parsed.callbacksPromises : method === 'validate' ? helper.dereferenced.callbacksPromises : helper[method + 'd'].callbacksPromises;
-          expect(parser.api).to.deep.equal(api);
-          expect(parser.$refs.paths()).to.deep.equal([path.abs('specs/callbacks-promises/callbacks-promises.yaml')]);
-          sinon.assert.calledWithExactly(callbackFn, null, result);
-          sinon.assert.calledWithExactly(thenFn, result);
-
-          done();
-        }
-        catch (e) {
-          done(e)
-        }
-      }, delay);
+            done();
+          }
+          catch (e) {
+            done(e);
+          }
+        }));
     }
   }
 
@@ -116,28 +114,27 @@ describe('Callback & Promise syntax', function() {
       var parser = new SwaggerParser();
       parser[method](path.rel('specs/callbacks-promises/callbacks-promises-error.yaml'), callbackFn)
         .then(thenFn)
-        .catch(catchFn);
+        .catch(catchFn)
+        .then(delay(function() {
+          try {
+            // Make sure the correct functions were called
+            sinon.assert.calledOnce(callbackFn);
+            sinon.assert.calledOnce(catchFn);
+            sinon.assert.notCalled(thenFn);
 
-      setTimeout(function() {
-        try {
-          // Make sure the correct functions were called
-          sinon.assert.calledOnce(callbackFn);
-          sinon.assert.calledOnce(catchFn);
-          sinon.assert.notCalled(thenFn);
+            // Make sure they were called with the correct values
+            expect(parser.api).to.deep.equal({swagger: 'ERROR'});
+            expect(parser.$refs.paths()).to.deep.equal([path.abs('specs/callbacks-promises/callbacks-promises-error.yaml')]);
+            var result = method === 'resolve' ? parser.$refs : sinon.match(parser.api);
+            sinon.assert.calledWithExactly(callbackFn, sinon.match.instanceOf(SyntaxError), result);
+            sinon.assert.calledWithExactly(catchFn, sinon.match.instanceOf(SyntaxError));
 
-          // Make sure they were called with the correct values
-          expect(parser.api).to.deep.equal({swagger: 'ERROR'});
-          expect(parser.$refs.paths()).to.deep.equal([path.abs('specs/callbacks-promises/callbacks-promises-error.yaml')]);
-          var result = method === 'resolve' ? parser.$refs : sinon.match(parser.api);
-          sinon.assert.calledWithExactly(callbackFn, sinon.match.instanceOf(SyntaxError), result);
-          sinon.assert.calledWithExactly(catchFn, sinon.match.instanceOf(SyntaxError));
-
-          done();
-        }
-        catch (e) {
-          done(e)
-        }
-      }, delay);
+            done();
+          }
+          catch (e) {
+            done(e);
+          }
+        }));
     }
   }
 });
