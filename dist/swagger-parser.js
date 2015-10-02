54789f45f1ced8b4440bf794cde3bdcb982950b7
James Messinger <james.messinger@gmail.com>
2015-10-02 16:05:19 -0500

    "release v3.3.0"

--- dist/swagger-parser.js
+++ dist/swagger-parser.js
@@ -1,6 +1,6 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SwaggerParser = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /** !
- * Swagger Parser v3.2.1
+ * Swagger Parser v3.3.0
  *
  * @link https://github.com/BigstickCarpet/swagger-parser
  * @license MIT
@@ -25077,7 +25077,7 @@ var SchemaValidators = {
         }
     },
     maxItems: function (report, schema) {
-        // http://json-schema.org/latest/json-schema-validation.html#rfc.section.5.3.2.1
+        // http://json-schema.org/latest/json-schema-validation.html#rfc.section.5.3.3.0
         if (typeof schema.maxItems !== "number") {
             report.addError("KEYWORD_TYPE_EXPECTED", ["maxItems", "integer"]);
         } else if (schema.maxItems < 0) {
